6ace9a72499d83743fbc6619168b73c8b1c44c40
Havinav <havinavdarishikpandilla@gmail.com>
2025-04-14 17:51:47 +0530

    bikes data updated

--- src/pages/ApplianceCard.jsx
+++ src/pages/ApplianceCard.jsx
@@ -1,65 +1,49 @@
-import React from "react";
+import React from 'react'
 
-const products = [
+const bikeData = [
   {
-    imageSrc:
-      "https://rukminim2.flixcart.com/image/312/312/xif0q/motorcycle/d/n/f/drum-self-kick-passion-drum-drum-97-2-booking-for-ex-showroom-original-imah6ceyz5ecdtys.jpeg?q=70",
-    color: "Black",
+    imgurl:
+      "https://rukminim2.flixcart.com/image/312/312/xif0q/motorcycle/f/n/j/i3s-self-kick-splendor-i3s-drum-97-2-booking-for-ex-showroom-original-imah6ceyypsearut.jpeg?q=70",
+    title: "Hero Spender",
   },
   {
-    imageSrc:
+    imgurl:
       "https://rukminim2.flixcart.com/image/300/300/xif0q/motorcycle/5/y/j/xtreme-125r-ibs-self-kick-xtreme-125r-ibs-disc-124-7-booking-for-original-imah6ceyjscqbefy.jpeg?q=90",
-    color: "Black",
+    title: "Hero Xtreme",
   },
   {
-    imageSrc:
-      "https://rukminim2.flixcart.com/image/300/300/xif0q/scooter-moped/c/n/4/-original-imah58qgyzdgyagq.jpeg?q=90",
-    color: "Blue",
+    imgurl:
+      "https://rukminim2.flixcart.com/image/300/300/xif0q/scooter-moped/u/o/v/zx-disc-xoom-zx-booking-for-ex-showroom-price-pan-india-hero-original-imahauhpv3yagaqd.jpeg?q=90",
+    title: "Hero XOOM",
   },
   {
-    imageSrc:
-      "https://rukminim2.flixcart.com/image/300/300/xif0q/scooter-moped/g/n/b/2025-drum-destini-prime-booking-for-ex-showroom-price-pan-india-original-imahauhzpkebhbhy.jpeg?q=90",
-    color: "White",
+    imgurl:
+      "https://rukminim2.flixcart.com/image/312/312/xif0q/electric-bike-scooter/9/s/0/-original-imah2ay3p9rqgjvn.jpeg?q=70",
+    title: "Lectrix LXS",
+  },
+  {
+    imgurl:
+      "https://rukminim2.flixcart.com/image/312/312/xif0q/electric-bike-scooter/j/m/e/ampere-reo-li-plus-portable-charger-reo-li-plus-drum-booking-for-original-imagx35z5w5ssddv.jpeg?q=70",
+    title: "Ampere Reo Li",
   },
 ];
 const ApplianceCard = () => {
   return (
     <div>
-      <div className="bg-white">
-        <div className="mx-auto max-w-2xl sm:px-6 sm:py-24 lg:max-w-7xl lg:px-9">
-          <h2 className="text-2xl font-bold tracking-tight text-gray-900">Bikes</h2>
-
-          <div className=" grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-            {products.map((product) => (
-              <div key={product.id} className="">
-                <img
-                  alt={product.imageAlt}
-                  src={product.imageSrc}
-                  className="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"
-                />
-                <div className="mt-4 flex justify-between">
-                  <div>
-                    <h3 className="text-sm text-gray-700">
-                      <a href={product.href}>
-                        <span aria-hidden="true" className="absolute inset-0" />
-                        {product.name}
-                      </a>
-                    </h3>
-                    <p className="mt-1 text-sm text-gray-500">
-                      {product.color}
-                    </p>
-                  </div>
-                  <p className="text-sm font-medium text-gray-900">
-                    {product.price}
-                  </p>
-                </div>
-              </div>
-            ))}
+      <div class="grid grid-cols-5 md:grid-cols-5 gap-4 bg-white">
+        {bikeData.map((item, index) => (
+          <div key={index} className="flex flex-col p-3">
+            <div>
+              <img src={item.imgurl} alt="" className="h-20 md:h-70 w-full" />
+            </div>
+            <h2 className="md:text-lg font-semibold text-black text-sm">
+              {item.title}
+            </h2>
           </div>
-        </div>
+        ))}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default ApplianceCard;
+export default ApplianceCard
